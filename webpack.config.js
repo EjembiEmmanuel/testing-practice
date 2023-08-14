3f04acae5c72234f78eea06ae1881ea0969ed03b
EjembiEmmanuel <ejembioche50@gmail.com>
2023-08-15 00:30:37 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "[name]",
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: ["node_modules"],
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: "./configs/app-config.js",
+      react: "./vendor/react-master",
+    },
+    extensions: [".js", ".jsx"],
+    modules: [
+      "node_modules",
+      "bower_components",
+      "shared",
+      "/shared/vendor/modules",
+    ],
+  },
+};
